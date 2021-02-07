2e4e9c0dbd815306d1bc070379bb9ddb649d6f5e
andresCev <>
2021-02-07 13:08:06 +0000

    listado

--- usuarios/ListView.swift
+++ usuarios/ListView.swift
@@ -4,30 +4,47 @@
 import Foundation
 import UIKit
 
-class ListView: UIViewController {
+class ListView: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var user:[User] = []
   
+    
+    @IBOutlet weak var listView: UITableView!
+    let alertService = AlertService()
     let request = Request()
+    var listas : [String] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        listView.dataSource = self
+        listView.delegate = self
         
 
-        request.getUsers(endpoint: "api/users") { [weak self] (result) in
-            
-            switch result{
-                
-            case.success(let lista):
-                print("imprimiendo desde getusers")
-                print(lista)
-               
-                
-                break
-              
-            case.failure(let error):
-               
-              print("error")
-                
-                break
+        request.getUsers(endpoint: "api/users") { [weak self] ( result ) in
+                  
+                       switch result{
+
+                       case.success(let lista):
+                           print("imprimiendo desde getusers")
+                        
+                          self!.listas = lista
+                          
+                          print(self!.listas)
+                          DispatchQueue.main.async {
+                              self!.listView.reloadData()
+                          }
+                         
+                         
+                        
+                       case.failure(let error):
+
+                           guard let alert = self?.alertService.alert(message: error.localizedDescription) else {
+                                   return
+                                   }
+                           self?.present(alert,animated: true)
+
+                           break
+
                 
             }
     
@@ -36,8 +53,19 @@ class ListView: UIViewController {
 }
     
 }
+    func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int
+        {
+            return listas.count
+        }
 
-
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = listView.dequeueReusableCell(withIdentifier: "cellId") as! userRow
+            print(listas[indexPath.row]+"hola")
+            cell.nombre.text = listas[indexPath.row]
+         
+          return cell
+        }
+        
 
     
 
---
+++ usuarios/userRow.swift
@@ -0,0 +1,15 @@
+//
+//  userRow.swift
+//  usuarios
+//
+//  Created by user177248 on 2/7/21.
+//
+
+import Foundation
+import UIKit
+class userRow: UITableViewCell {
+
+    @IBOutlet weak var nombre: UILabel!
+    
+
+}
